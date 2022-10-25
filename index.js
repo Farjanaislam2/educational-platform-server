60cf03f3813cbe41314037d4bef4bc310a23b4fd
Farjana Islam <farjanaanika2@gmail.com>
2022-10-25 08:07:28 -0700

    final

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const port =process.env.port || 5000;
 
 app.use(cors());
 
-const courses= require('./data/courses.json') 
+const courses= require('./data/courses.json')
+const details = require('./data/course-detail.json') 
 
 app.get('/', (req, res) => {
     res.send('api rinning')
 }) ;
+
 app.get('/course-details', (req, res) => {
     res.send(courses)
 }) ;
+
+app.get('/details/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedDetails = details.find(n => n._id ===id);
+    res.send(selectedDetails);
+})
 
 
 app.listen(port, () => {
