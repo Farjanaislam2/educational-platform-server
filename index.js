6493493d864c7ebb30a0ff1748be98f438fec3c9
Farjana Islam <farjanaanika2@gmail.com>
2022-10-25 02:32:26 -0700

    fake-data

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app =express();
 const port =process.env.port || 5000;
 
+
+const courses= require('./data/courses.json') 
+
 app.get('/', (req, res) => {
     res.send('api rinning')
+}) ;
+app.get('/course-details', (req, res) => {
+    res.send(courses)
 }) ;
 
 
